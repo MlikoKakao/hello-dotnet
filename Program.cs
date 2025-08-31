965befa7fc6d8d80afe9e2c4138e7b94886f8199
MlikoKakao <mattewczlp@seznam.cz>
2025-08-31 11:44:40 +0200

    feat: getting used to Nvim, Linux, C#, calc app

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿	using System;
+
+	// See https://aka.ms/new-console-template for more information
+	namespace Calc
+	{
+	    class Program
+	    {
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("Enter first number: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter operator (+ - * /): ");
+                string op = Console.ReadLine();
+
+                Console.WriteLine("Enter second number: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                double result = op switch
+                {
+                    "+" => a + b,
+                    "-" => a - b,
+                    "*" => a * b,
+                    "/" => b != 0 ? a / b : double.NaN,
+
+                    _ => double.NaN
+                };
+
+                Console.WriteLine($"Results: {result}");
+                Console.WriteLine();
+
+            }
+        }
+    }
+
+}
+
