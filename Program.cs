37cb6e841978d9e3093ef965876ee110388406b2
MlikoKakao <mattewczlp@seznam.cz>
2025-09-02 20:43:50 +0200

    simple password generator

--- Program.cs
+++ Program.cs
@@ -6,6 +6,84 @@ namespace Calc
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("How many characters should your password have?");
+            string scannedLength = Console.ReadLine();
+            int length = 0;
+            Int32.TryParse(scannedLength, out length);
+            Console.WriteLine("Do you want special characters? Yes or no");
+            string spec = "";
+            spec = Console.ReadLine().ToLower();
+            bool special = spec == "yes";
+            PasswordGenerator(length, special);
+
+        }
+
+
+
+        static string PasswordGenerator(int length, bool special)
+        {
+            string characters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
+            string specialCharacters = "~!@#$%^&*-_=+|?/><";
+            Random random = new Random();
+            string password = "";
+            int index = 0;
+
+            if (special)
+            {
+                characters += specialCharacters;
+
+                for (int i = 0; i < length; i++)
+                {
+                    index = random.Next(characters.Length);
+                    password += characters[index];
+                }
+                Console.WriteLine("Password is: " + password);
+                return password;
+
+            }
+            for (int i = 0; i < length; i++)
+            {
+                index = random.Next(characters.Length);
+                password += characters[index];
+            }
+            Console.WriteLine("Password is: " + password);
+            return password;
+
+        }
+
+
+        static double Calculator()
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter first number: ");
+
+                double a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter operator (+ - * /): ");
+                string op = Console.ReadLine();
+
+                Console.WriteLine("Enter second number: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+
+                double result = op switch
+                {
+                    "+" => a + b,
+                    "-" => a - b,
+                    "*" => a * b,
+                    "/" => b != 0 ? a / b : double.NaN,
+                    _ => double.NaN
+                };
+
+                Console.WriteLine($"Results: {result}");
+                Console.WriteLine();
+
+            }
+        }
+
+        static void ParserLINQ()
+        {
+
             using (TextFieldParser parser = new TextFieldParser(@"data.csv"))
             {
                 parser.TextFieldType = FieldType.Delimited;
@@ -37,77 +115,9 @@ namespace Calc
                 Console.WriteLine(floatingNumber);
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        static double Calculator()
-        {
-            while (true)
-            {
-                Console.WriteLine("Enter first number: ");
-
-                double a = Convert.ToDouble(Console.ReadLine());
-
-                Console.WriteLine("Enter operator (+ - * /): ");
-                string op = Console.ReadLine();
-
-                Console.WriteLine("Enter second number: ");
-                double b = Convert.ToDouble(Console.ReadLine());
-
-                double result = op switch
-                {
-                    "+" => a + b,
-                    "-" => a - b,
-                    "*" => a * b,
-                    "/" => b != 0 ? a / b : double.NaN,
-                    _ => double.NaN
-                };
-
-                Console.WriteLine($"Results: {result}");
-                Console.WriteLine();
-
-            }
-        }
     }
-
 }
+
+
+
 
